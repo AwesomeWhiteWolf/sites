cf881a7100b9cd30beb021fe9c6dbf9a94a8e2a8
AwesomeWhiteWolf <148298503+AwesomeWhiteWolf@users.noreply.github.com>
2024-02-08 10:06:42 +0800

    Add files via upload

--- login-page/script.js
+++ login-page/script.js
@@ -1,32 +1,11 @@
-let expenses = [];
 
-function addExpense() {
- let item = document.getElementById("item").value;
- let cost = document.getElementById("cost").value;
-
- if (item && cost) {
-  expenses.push({item, cost});
-  document.getElementById("expenseTable").innerHTML += "<tr><td>" + item + "</td><td>$" + cost + "</td><td><button onclick='deleteExpense(" + (expenses.length - 1) + ")'>Delete</button></td></tr>";
-  document.getElementById("total").innerHTML = "Total: $" + getTotal();
-  document.getElementById("item").value = "";
-  document.getElementById("cost").value = "";
- }
+function myFunction() {
+  let x = document.getElementById("myInput");
+  x.innerText = "2";
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
-
-function deleteExpense(index) {
- expenses.splice(index, 1);
- document.getElementById("expenseTable").deleteRow(index + 1);
- document.getElementById("total").innerHTML = "Total: $" + getTotal();
-}
-
-function getTotal() {
- let total = 0;
-
- for (let i = 0; i < expenses.length; i++) {
-  total += parseInt(expenses[i].cost);
- }
-
- return total;
-}
-
 
